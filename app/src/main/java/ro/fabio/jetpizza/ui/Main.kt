114ba3d3cbed7646b12f2cdbd25ad0c9010f743c
alexivxii <alex.munteanu58@gmail.com>
2023-08-18 20:01:01 +0300

    navigation working

--- app/src/main/java/ro/fabio/jetpizza/ui/Main.kt
+++ app/src/main/java/ro/fabio/jetpizza/ui/Main.kt
@@ -8,9 +8,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import ro.fabio.jetpizza.MainActivity
 import ro.fabio.jetpizza.model.Pizza
 import ro.fabio.jetpizza.model.allPizzas
@@ -22,22 +24,27 @@ import ro.fabio.jetpizza.ui.home.HomeScreen
 @Composable
 fun Main() {
     val navController = rememberNavController();
-    NavHost(navController = navController, startDestination = "pizzaconfig"){
+    NavHost(navController = navController, startDestination = "home"){
         composable(
             route = "home"
         ){
             HomeScreen(
                 pizzas = allPizzas,
-                onPizzaSelected = { /* to be implemented */ pizza : Pizza -> Log.d("TagTestPizzaClick","Clicked") }   //every item of type Pizza does -> function
+                onPizzaSelected = { /* to be implemented */ pizza : Pizza -> navController.navigate("pizzaconfig/${pizza.id}") }   //every item of type Pizza does -> function
             )
         }
 
         composable(
-            route = "pizzaconfig"
-        ){
-            ConfigureScreen(
-                selectedPizza = allPizzas.first()
+            route = "pizzaconfig/{pizzaId}",
+            arguments = listOf(
+                navArgument("pizzaId") {type = NavType.LongType}
             )
+        ){
+            backstackEntry ->
+            val args = backstackEntry.arguments
+            val pizzaId = args?.getLong("pizzaId")
+            ConfigureScreen(selectedPizza = allPizzas.first{pizza -> pizza.id == pizzaId})
+//            ConfigureScreen(selectedPizza = allPizzas.first())
         }
     }
 
